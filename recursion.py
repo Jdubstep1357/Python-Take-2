359ea3d699eda7797a0f8bb053a3997c18b8efb2
John Lyons <jwlyons2015@gmail.com>
2021-10-24 16:41:27 -0500

    Coding Challenge: Reversals

--- recursion.py
+++ recursion.py
@@ -4,3 +4,7 @@ def rFib(n):
     elif n == 1 or n == 2:
         return 1
     return rFib(n-1) + rFib(n-2)
+
+# to access python shell in interactive mode, type in python -i recursion.py
+# then to access use rFib(2) or whatever number to put in
+# to quit out of interactive mode use command z
