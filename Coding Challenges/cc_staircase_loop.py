219e293fbb5bf2d3a57ccaef46bfc7344bbdd0c8
John Lyons <jwlyons2015@gmail.com>
2021-10-24 13:12:10 -0500

    BuiltIn Functions

--- Coding Challenges/cc_staircase_loop.py
+++ Coding Challenges/cc_staircase_loop.py
@@ -2,4 +2,4 @@ stars = ""
 for i in range(0, 5, 1):
     for j in range(0, i, 1):
         stars += "*"
-    print(stars)
+print(stars)
---
+++ builtin_functions.py
@@ -0,0 +1,22 @@
+print("Math Functions")
+
+abs_int = abs(-1)
+print(abs_int)
+
+int_to_float = float(100)
+print(int_to_float)
+
+float_to_int = int(1.23)
+print(float_to_int)
+
+# prints out biggest number
+print(max(1, 2, -5, 10, 0))
+
+# prints out smallest number
+print(min(1, 2, -5, 10, 0))
+
+#prints out power of object
+print(pow(2, 3))
+
+#rounds nearest ineger
+print(round(51.6))
